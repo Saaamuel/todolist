158dbcccdde23fab37ba57a9cb118f8a2f2d1ee5
Saaamuel <samuelcosta233@gamil.com>
2024-12-19 16:08:23 -0300

    Descrição do que foi alterado

---
+++ frontend/src/components/App.js
@@ -0,0 +1,14 @@
+//Junta os componentes TaskList e TaskForm.
+
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Task Manager</h1>
+        </div>
+    );
+};
+
+export default App;
+
